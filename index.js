b50dbdbd559ffbdc1c078cf99397c67ca7df38bb
pamini932 <122174867+pamini932@users.noreply.github.com>
2023-02-16 12:54:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require(`discord.js`);
-
+const Discord = require (`disccord.js`);
 const prefix = '!';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, 
@@ -66,41 +66,68 @@ if (command === 'createtextchannel') {
         channel.setParent(categoryId)
     })
 }
-//testing poll command
-client.on('message', message => {
-    
-    /*if (message.content.startsWith('!poll')) {*/
-    if (command === 'poll') {
-        const args = message.content.slice(6).split('|');
-        const question = args[0];
-        const options = args.slice(1);
-    if (options.length < 2) {
-        return message.reply('Please provide at least 2 options for the poll.');
-        }
-    const pollEmbed = new Discord.MessageEmbed()
-        .setTitle(question)
-        .setColor('#0099ff')
-        .setFooter('Poll created by ' + message.author.username)
-        .setTimestamp();
-    
-        options.forEach((option, i) => {
-            pollEmbed.addField(`Option ${i + 1}`, option);
-            });
+//test poll command
+const {Client, GatewayIntentBits, EmbedBuilder, PermissionsBitFields, Permissions }=require (`discord.js`);
+const Discord = require(`discord.js`);
 
-    message.channel.send(pollEmbed)
-        .then(pollMessage => {
-            options.forEach((option, i) => {
-            pollMessage.react(['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯'][i]);
+const prefix = '!';
+
+const client2 = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+  
+  client2.on('message', message => {
+    if (message.content === '!poll') {
+      const filter = m => m.author.id === message.author.id;
+      let question, options;
+  
+      message.reply('Please enter the poll question and options in the following format: "Question,Option 1,Option 2,Option 3"')
+      .then(() => {
+        message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+          .then(collected => {
+            const input = collected.first().content;
+            const inputArray = input.split(',');
+  
+            if (inputArray.length < 4) {
+              throw new Error('Invalid input'); // Throw an error if the user's input is not in the correct format
+            }
+  
+            question = inputArray[0];
+            options = inputArray.slice(1);
+  
+            const pollEmbed = new Discord.MessageEmbed()
+              .setColor('#0099ff')
+              .setTitle(question)
+              .setDescription('Vote for your favorite option!');
+  
+            for (let i = 0; i < options.length; i++) {
+              pollEmbed.addField(`Option ${i + 1}`, options[i], true);
+            }
+  
+            message.channel.send(pollEmbed)
+            .then(pollMessage => {
+              for (let i = 0; i < options.length; i++) {
+                const emoji = getEmoji(i + 1);
+                pollMessage.react(emoji);
+              }
             });
-        })
-        .catch(console.error);
+          })
+          .catch(() => {
+            message.reply('You did not enter a poll question and options in time, or your input was invalid. Please try again.');
+          });
+      });
     }
-    
-})
-
+  });
+  
+  // Helper function to get the emoji based on the option number
+  function getEmoji(optionNumber) {
+    const emojiArray = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'];
+    return emojiArray[optionNumber - 1];
+  }
+  
 
     }))
 
 
 
-client.login(process.env.BOT_TOKEN);
+client.login();
+client2.login(process.env.BOT_TOKEN);
